282886f08e718a40889314c628e8f00686937738
Ben <benjamin.lammel@students.fhnw.ch>
2015-06-12 13:25:14 +0200

    changed msp430-47 source

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,13 +9,13 @@ wget -O - http://tinyprod.net/repos/debian/tinyprod.key | sudo apt-key add -
 #echo "deb http://tinyprod.net/repos/debian squeeze main" >> /etc/apt/sources.list
 
 echo "deb http://tinyprod.net/repos/debian squeeze main" >> /etc/apt/sources.list
-echo "deb http://tinyprod.net/repos/debian msp430-46 main" >> /etc/apt/sources.list
+echo "deb http://tinyprod.net/repos/debian msp430-47 main" >> /etc/apt/sources.list
   
 # force update
 apt-get update
 
 # install tinyos-tools
-apt-get install -y --force-yes msp430-46 nesc tinyos-tools
+apt-get install -y --force-yes msp430-47 nesc tinyos-tools
 
 echo "source /vagrant/tinyos-2.1.2/tinyos.sh" >> /home/vagrant/.bashrc
 
